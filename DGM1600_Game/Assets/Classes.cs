c8f6615448680dec236a103d0cc1e41337461b03
Unknown <wade.edwards11@gmail.com>
2017-08-30 20:11:23 -0600

    Add Unity Project Files

---
+++ DGM1600_Game/Assets/Classes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Classes : MonoBehaviour {
+
+	public class One {
+		public int money = 100000000;
+		public float fuel = 0.01f;
+		private string name = "Bob";
+
+	}
+	
+	public class Two {
+		public int initialAmmo = 25;
+		public int initialGrenades = 2;
+		public int initialArmour = 25;
+	}
+
+	public class Three {
+		public float initialFuel = .75f;
+		public int inititalHealth = 100;
+	}
+}
