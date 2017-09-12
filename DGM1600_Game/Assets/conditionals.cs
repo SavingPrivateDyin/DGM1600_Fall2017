8de430deef24018c2ea3d398f85b3d7e06e95498
Unknown <wade.edwards11@gmail.com>
2017-09-11 20:33:49 -0600

    two assignments

---
+++ DGM1600_Game/Assets/conditionals.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class conditionals : MonoBehaviour {
+
+	public string stopLight = "Red";
+
+	void Start()
+	{
+		Main();
+	}
+	void Main(){
+		
+		if(stopLight == "Red"){
+			print("STOP! The light is Red!");
+	}
+		else if(stopLight == "Yellow"){
+			print("The Light is Yellow. SLOW DOWN!!!");
+		}
+		else if(stopLight == "Green"){
+			print("Green Means GO!");
+		}
+		else{
+			print("Are you sure you are at a Stop Light?!");
+		}
+
+	}
+	
+
+
+}
