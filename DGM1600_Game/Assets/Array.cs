7896a153885bd7b5507c01dd3cf411bd5050fa2b
Unknown <wade.edwards11@gmail.com>
2017-09-20 20:19:57 -0600

    Arrays assingment

---
+++ DGM1600_Game/Assets/Array.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Array : MonoBehaviour {
+
+	public string[] heros = new string[6];
+	
+	
+	// Use this for initialization
+	void Start () {
+
+		heros[0] = "Batman";
+		heros[1] = "Superman";
+		heros[2] = "Supidaman";
+		heros[3] = "Catwoman";
+		heros[4] = "YourMom";
+		heros[5] = "CaptainUnderPants";
+
+		//print(heros[3]);
+		for(int i=0; i<5; i++){
+			print(heros[i]);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
