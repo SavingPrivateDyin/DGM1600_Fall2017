8bf810588c8d984fde615aef8c9a03aa20960684
Unknown <wade.edwards11@gmail.com>
2017-10-11 20:22:04 -0600

    For Each Loops

---
+++ DGM1600_Game/Assets/ForEach.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForEach : MonoBehaviour {
+	
+	
+	// Use this for initialization
+	void Start () {
+		List<BadGuy> badguys = new List<BadGuy>();
+
+		badguys.Add( new BadGuy("Snoke", 1000));
+		badguys.Add( new BadGuy("Magneto", 50));
+		badguys.Add( new BadGuy("Your Mom", -2));
+
+		badguys.Sort();
+
+		foreach(BadGuy guy in badguys){
+			print (guy.name + " " + guy.power);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
